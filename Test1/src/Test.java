e32b3bbb222d2eec037850dfca6837f75fdedb19
Cvqtko <cvqtkokirilov@gmail.com>
2017-04-13 20:59:41 +0300

    pff

--- Test1/src/Test.java
+++ Test1/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 	public static void main(String[] args) {
 
-		System.out.println("Hello World Changed By Elka :P");
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
 
 	}
 }

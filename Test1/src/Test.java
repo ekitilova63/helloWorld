8dcc63ee345719a4b04797c900d715153dccc0b3
ekitilova63 <kirilkirilovkirilovkirilov@gmail.com>
2017-04-13 20:54:20 +0300

    Update Test.java

--- Test1/src/Test.java
+++ Test1/src/Test.java
@@ -1,6 +1,6 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello World Changed By Elka");
+		System.out.println("Hello World Changed By Elka :)");
 	}
 }

47397ab88965adda54a5ac56a35eea41965ce762
Cvqtko <cvqtkokirilov@gmail.com>
2017-04-13 21:02:49 +0300

    Tesing conflicts

--- Test1/src/Test.java
+++ Test1/src/Test.java
@@ -3,6 +3,36 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
 
 	}
 }

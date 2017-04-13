595c33e28f75af6df1a603e24638b98fe0819536
ekitilova63 <kirilkirilovkirilovkirilov@gmail.com>
2017-04-13 21:02:20 +0300

    Update Test.java

--- Test1/src/Test.java
+++ Test1/src/Test.java
@@ -2,7 +2,37 @@
 public class Test {
 	public static void main(String[] args) {
 
-		System.out.println("Hello World Changed By Elka :P pleze6to se 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
+		System.out.println("Hello World Changed By Elka :( tujno 4ove4e");
 
 	}
 }
